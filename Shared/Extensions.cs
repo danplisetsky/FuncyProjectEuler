8718fe802a653fdad0a910c98bab4c11e4664b59
Daniel Plisetsky <danplisetsky@gmail.com>
2017-08-14 13:35:03 +0300

    Add shared class library and tests
    Solve problem #2

---
+++ Shared/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public static class Extensions
+    {
+        #region int 
+
+        public static bool IsEven(this int x) =>
+            x % 2 == 0;
+
+        #endregion int
+
+    }
+}
---
+++ Tests/TestExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestExtensions
+    {
+        [TestMethod]
+        public void IsEven()
+        {
+            int n = 4;
+
+            bool result = n.IsEven();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
