f60e9d68f66147ac78a3066857cfa92db07ce15b
Daniel Plisetsky <danplisetsky@gmail.com>
2017-08-14 14:43:36 +0300

    Solve problem #3
    Add StrangeEnumerable class, extension methods and tests for problem #3

--- Tests/TestExtensions.cs
+++ Tests/TestExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shared;
 
@@ -8,13 +9,74 @@ namespace Tests
     public class TestExtensions
     {
         [TestMethod]
-        public void IsEven()
+        public void IntIsEven()
         {
             int n = 4;
 
             bool result = n.IsEven();
 
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IntIsNotEven()
+        {
+            int n = 5;
+
+            bool result = n.IsEven();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void LongIsEven()
+        {
+            long n = long.MaxValue - 1;
+
+            bool result = n.IsEven();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void LongIsNotEven()
+        {
+            long n = long.MaxValue;
+
+            bool result = n.IsEven();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void LongIsPrime()
+        {
+            long n = 922_337_203_369;
+
+            bool result = n.IsPrime();
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void LongIsNotPrime()
+        {
+            long n = 922_337_203_367;
+
+            bool result = n.IsPrime();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GetPrimeFactors()
+        {
+            long n = 10;
+
+            long result = n.GetPrimeFactors().Last();
+
+            Assert.AreEqual(result, 5);
         }
     }
 }
---
+++ pn3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Shared;
+/*
+* The prime factors of 13195 are 5, 7, 13 and 29.
+* What is the largest prime factor of the number 600851475143 ?
+*/
+
+namespace pn3
+{
+    class Program
+    {
+        const long NUMBER = 600_851_475_143;
+
+        static void Main(string[] args)
+        {
+            long result = NUMBER.GetPrimeFactors().Last();
+            Console.WriteLine(result);
+        }
+    }
+}
