25193b6aacc1224fa2caac7dde2aef0875ff4965
Daniel Plisetsky <danplisetsky@gmail.com>
2017-08-18 10:47:33 +0300

    Solve problem #7

---
+++ pn7/Program.cs
@@ -0,0 +1,25 @@
+﻿/* By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * What is the 10 001st prime number?
+ */
+
+using System.Linq;
+using Shared;
+
+namespace pn7
+{
+    class Program
+    {
+        const int COUNT = 10001;
+
+        static void Main(string[] args)
+        {
+            long result = GetPrimeNumber(COUNT);
+            System.Console.WriteLine(result);
+        }
+
+        private static long GetPrimeNumber(int count) =>
+            StrangeEnumerable.RangePrimes(long.MaxValue)
+            .Take(count)
+            .Last();
+    }
+}
