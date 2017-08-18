8ec426b4e5040737f67609c8bb0158525a7974fc
Daniel Plisetsky <danplisetsky@gmail.com>
2017-08-18 11:36:10 +0300

    Solve problem #10

---
+++ pn10/Program.cs
@@ -0,0 +1,20 @@
+﻿/* The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+
+using System.Linq;
+using Shared;
+
+namespace pn10
+{
+    class Program
+    {
+        const int CEILING = 2_000_000;
+
+        static void Main(string[] args)
+        {
+            long result = StrangeEnumerable.RangePrimes(CEILING).Sum();
+            System.Console.WriteLine(result);
+        }
+    }
+}
